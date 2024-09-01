f214f313ab7859c8031ea85e63f392df05458fd4
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-01 05:53:14 +0200

    chore(examples): only save identification since configuration can be retrieved finally

--- examples/_persisted-session.ts
+++ examples/_persisted-session.ts
@@ -1,23 +1,15 @@
-import type { Configuration, Identification } from "../src";
+import type { Identification } from "../src";
 
 import fs from "node:fs/promises";
 import path from "node:path";
 
 const json = path.join(__dirname, "_persisted-session.json");
 
-export const persist = async (identification: Identification, configuration: Configuration): Promise<void> => {
-  await fs.writeFile(json, JSON.stringify({ configuration, identification }), "utf8");
+export const persist = async (identification: Identification): Promise<void> => {
+  await fs.writeFile(json, JSON.stringify(identification), "utf8");
 };
 
-export const read = async (): Promise<{
-  identification: Identification
-  configuration: Configuration
-}> => {
+export const read = async (): Promise<Identification> => {
   const data = await fs.readFile(json, "utf8");
-  const parsed = JSON.parse(data);
-
-  if (!parsed.identification || !parsed.configuration)
-    throw new Error("You must authentication using 'mobile.ts' before !");
-
-  return parsed;
+  return JSON.parse(data);
 };
