4722256bcd050044d8a53e3d80418de11587eb50
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-01 05:52:29 +0200

    refactor(configuration): properties

--- src/models/configuration.ts
+++ src/models/configuration.ts
@@ -1,18 +1,13 @@
 export type Configuration = Readonly<{
   currency: string
 
-  p2pPayMin: number
-  p2pPayMax: number
-  p2pPayPartMin: number
-  p2pPayPartMax: number
+  paymentMinimum: number
+  paymentMaximum: number
+  paymentPartMinimum: number
+  paymentPartMaximum: number
 
-  moneyInMin: number
-  moneyInMax: number
-  moneyOutMin: number
-  moneyOutMax: number
-
-  p2pRequestAmount: number
-  p2pGetAmount: number
-
-  transactionAmount: number
+  moneyInMinimum: number
+  moneyInMaximum: number
+  moneyOutMinimum: number
+  moneyOutMaximum: number
 }>;
