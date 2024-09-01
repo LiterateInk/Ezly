d6b1d0d25f31a62bdf02cd63204a9cce46b38a79
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-01 05:54:06 +0200

    docs(examples): use `information()` to get `configuration`

--- examples/balance.ts
+++ examples/balance.ts
@@ -2,9 +2,12 @@ import * as izly from "../src";
 import { read } from "./_persisted-session";
 
 void async function main () {
-  const { configuration, identification } = await read();
+  const identification = await read();
 
   const balance = await izly.balance(identification);
+  // note that information also gives balance but let's not use it here !
+  const { configuration } = await izly.information(identification);
+
   console.log("Your balance is currently at", balance.value, configuration.currency);
   console.log("Your cash balance is currently at", balance.cashValue, configuration.currency);
   console.log(`Last updated the ${balance.lastUpdate.toLocaleString("fr-FR")}`);
