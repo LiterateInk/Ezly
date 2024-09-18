dc1721424361ccf032a0ccfde5a3e814043c106c
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-18 08:43:24 +0200

    feat(api): implement qr code payment

---
+++ examples/generate-pay-qr-code.ts
@@ -0,0 +1,13 @@
+import * as izly from "../src";
+import { read } from "./_persisted-session";
+import { toString as qrcode } from "qrcode";
+
+void async function main () {
+  const identification = await read();
+  const data = izly.qrPay(identification);
+
+  // show the qr code in the terminal
+  qrcode(data, { type: "utf8" }, (_, qr) => {
+    console.log(qr);
+  });
+}();
