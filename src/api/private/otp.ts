d09f009b957fa29ac88def3ea16264bf143876aa
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-18 08:43:13 +0200

    refactor(otp): use new functions

--- src/api/private/otp.ts
+++ src/api/private/otp.ts
@@ -1,18 +1,8 @@
-import forge from "node-forge";
+import { base64, base64url } from "@scure/base";
 import { packBigEndian } from "~/core/pack";
+import { hashWithHMAC } from "~/core/hmac";
 
 export const otp = (seed: string, counter: number): string => {
-  const key = forge.util.decode64(seed);
   const packedCounter = packBigEndian(counter);
-
-  const hmac = forge.hmac.create();
-  hmac.start("sha1", key);
-  hmac.update(packedCounter);
-
-  let output = forge.util.encode64(hmac.digest().getBytes());
-  output = output
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-
-  return output;
+  return base64url.encode(hashWithHMAC(packedCounter, base64.decode(seed)));
 };
