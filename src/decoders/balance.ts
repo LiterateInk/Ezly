8cbf5b06b363f61d238d30e9095de7594bbd8ed1
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-08 00:58:29 +0100

    fix(decoders): ignore ts errors because of the "maybe string" weird condition

--- src/decoders/balance.ts
+++ src/decoders/balance.ts
@@ -1,14 +1,18 @@
+import type { UP } from "~/definitions/up";
 import type { Balance } from "~/models";
 
-export const decodeBalance = (data: any): Balance => {
+export const decodeBalance = (data: UP): Balance => {
   // ex: 31-08-2024 04:57:45
   const [date, time] = data.LUD.split(" ") as string[];
   // date is reversed for proper parsing, we reverse it manually.
   const reversedDate = date.split("-").reverse().join("-");
 
   return {
+    // @ts-expect-error : sometimes might be a string...
     value: parseFloat(data.BAL),
+    // @ts-expect-error : sometimes might be a string...
     cashValue: parseFloat(data.CASHBAL),
+    // @ts-expect-error : sometimes might be a string...
     g7CardValue: parseFloat(data.G7CARDBAL ?? 0),
     lastUpdate: new Date(`${reversedDate} ${time}`)
   };
