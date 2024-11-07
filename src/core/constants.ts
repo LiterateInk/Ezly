40c6d27ae8398ed4702696ff8dd4dcdb518dd580
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-08 00:58:10 +0100

    chore: add a REST route generator

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -3,3 +3,5 @@ export const CLIENT_TYPE = "PART";
 
 export const SOAP_URL = new URL("https://soap.izly.fr/Service.asmx");
 export const SOAP_USER_AGENT = "ksoap2-android/2.6.0+"; // mimick the kotlin app
+
+export const createRouteREST = (action: string) => new URL(`https://rest.izly.fr/Service/PublicService.svc/rest/${action}`);
