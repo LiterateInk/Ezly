a21f595842f629c8b0811fe74131340d3c32a81d
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-08 00:56:25 +0100

    chore: write a more complete example for operations

--- examples/operations.ts
+++ examples/operations.ts
@@ -4,18 +4,22 @@ import { read } from "./_persisted-session";
 void async function main () {
   const identification = await read();
 
-  const payments = await izly.operations(identification, izly.OperationKind.Payment, 5);
-  const topup = await izly.operations(identification, izly.OperationKind.TopUp, 5);
+  const payments = await izly.operations(identification, izly.TransactionGroup.Payments, 5);
+  const topup = await izly.operations(identification, izly.TransactionGroup.TopUp, 5);
 
-  console.info("- Payments -");
+  console.info("--- Payments");
   for (const payment of payments) {
     const priceAsString = `${payment.amount.toPrecision(3)} EUR`;
-    console.info(payment.date.toLocaleString("fr-FR"), "paid", priceAsString);
+    console.info("Paid", priceAsString, "the", payment.date.toLocaleString("fr-FR") );
+    console.info("=> (type):", izly.OperationType[payment.type], `(${payment.type})`);
+    console.info("=> (status):", izly.TransactionGroupStatus[payment.status], `(${payment.status})`);
   }
 
-  console.info("- Top-up -");
+  console.info("\n--- TopUp");
   for (const operation of topup) {
     const priceAsString = `${operation.amount.toPrecision(3)} EUR`;
-    console.info(operation.date.toLocaleString("fr-FR"), "received", priceAsString);
+    console.info("Received", priceAsString, "the", operation.date.toLocaleString("fr-FR"));
+    console.info("=> (type):", izly.OperationType[operation.type], `(${operation.type})`);
+    console.info("=> (status):", izly.TransactionGroupStatus[operation.status], `(${operation.status})`);
   }
 }();
